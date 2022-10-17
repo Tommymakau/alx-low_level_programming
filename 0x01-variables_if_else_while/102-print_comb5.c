d2ea63a2ca415159c5631c6c273ed21cee6c0da2
tommymakau <bryanmutso@gmail.com>
2022-10-17 09:53:31 -0700

    k

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/* Software is eating the World*/
+/* Inventing*/
 /**
  * main - Entry point
  *
@@ -10,43 +10,37 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i;
+	int j;
 
-	i = j = k = 48; l = 49;
-	while  ((i < 58))
+	i = 48;
+	j = 48;
+	while  ((i < 58) && (j < 58))
 	{
-		putchar(i); putchar(j); putchar(32); putchar(k); putchar(l);
-		if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+		putchar(i);
+		putchar(j);
+		if ((i == 57) && (j == 57))
 		{
-			putchar('\n'); i++;
+			putchar('\n');
+			i++;
+			j++;
 		}
 		else
 		{
-			putchar(44); putchar(32);
-			if ((k == 57) && (l == 57))
+			putchar(44);
+			putchar(32);
+			if (j < 57)
 			{
-				if (j < 56)
-				{
-					l = ++j + 1; k = i;
-				}
-				else if (j == 56)
-				{
-					j++; k = i + 1; l = 48;
-				}
-				else if (j == 57)
-				{
-					j = 48; l = 49; k = ++i;
-				}
-			}
-			else if (l < 57)
-			{
-				l++;
+				j++;
 			}
 			else
 			{
-				l = 48; k++;
+				j = 48;
+				i++;
 			}
 		}
+
 	}
+
 	return (0);
 }
